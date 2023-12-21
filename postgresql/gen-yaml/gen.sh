fdb3bde74aae777b04b3541bf70b8b77f74f06e0
oliverpauffley <mrpauffley@gmail.com>
2023-12-21 11:14:57 +0000

    patch bitnami

--- postgresql/gen-yaml/gen.sh
+++ postgresql/gen-yaml/gen.sh
@@ -4,13 +4,14 @@
 set -o errexit -o pipefail -o nounset
 
 mkdir -p manifests/upstream
+rm manifests/upstream/*
 
 # Fetch from helm repo: https://github.com/bitnami/charts/tree/main/bitnami/postgresql
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
-helm template "${NAME}" bitnami/postgresql --version "${BITNAMI_POSTGRES_RELEASE}" \
-  --set fullnameOverride="${NAME}" \
-  --set nameOverride="${NAME}" \
+helm template "postgres" bitnami/postgresql --version "${BITNAMI_POSTGRES_RELEASE}" \
+  --set fullnameOverride="postgres" \
+  --set nameOverride="postgres" \
   --set commonAnnotations."app\.uw\.systems\/description"="" \
   --set commonAnnotations."app\.uw\.systems\/tier"="" \
   --set commonAnnotations."app\.uw\.systems\/repos"="https://github.com/utilitywarehouse/shared-kustomize-bases/tree/main/postgresql" \
@@ -18,4 +19,7 @@ helm template "${NAME}" bitnami/postgresql --version "${BITNAMI_POSTGRES_RELEASE
   --set master.resources.limits.cpu="1000m" \
   --set master.resources.requests.memory="0" \
   --set master.resources.limits.memory="1Gi" \
-  --set backup.enabled="false" > "manifests/upstream/postgres.yaml"
+  --set backup.enabled="false" > manifests/upstream/postgres.yaml
+
+cp gen-yaml/kustomization.yaml manifests/upstream/kustomization.yaml
+cp gen-yaml/delete-secret.yaml manifests/upstream/delete-secret.yaml
