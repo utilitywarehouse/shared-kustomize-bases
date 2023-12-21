464f082b4b3aa4af662d222ffcf11a6ab997e7b9
oliverpauffley <mrpauffley@gmail.com>
2023-12-21 16:08:46 +0000

    feat: add metrics

--- postgresql/gen-yaml/gen.sh
+++ postgresql/gen-yaml/gen.sh
@@ -4,7 +4,7 @@
 set -o errexit -o pipefail -o nounset
 
 mkdir -p manifests/upstream
-rm manifests/upstream/*
+rm -f manifests/upstream/*
 
 # Fetch from helm repo: https://github.com/bitnami/charts/tree/main/bitnami/postgresql
 helm repo add bitnami https://charts.bitnami.com/bitnami
@@ -18,10 +18,12 @@ helm template "postgresql" bitnami/postgresql --version "${BITNAMI_POSTGRES_RELE
   --set auth.existingSecret="postgresql" \
   --set auth.secretKeys.adminPasswordKey="admin-password" \
   --set auth.secretKeys.userPasswordKey="user-password" \
+  --set auth.database="database" \
   --set master.resources.requests.cpu="0" \
   --set master.resources.limits.cpu="1000m" \
   --set master.resources.requests.memory="0" \
   --set master.resources.limits.memory="1Gi" \
+  --set metrics.enabled="true" \
   --set backup.enabled="false" > manifests/upstream/postgres.yaml
 
 cp gen-yaml/kustomization.yaml manifests/upstream/kustomization.yaml
