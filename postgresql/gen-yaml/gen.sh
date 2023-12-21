e231f385535c0b1d5be136979ec75eb641da8ec3
oliverpauffley <mrpauffley@gmail.com>
2023-12-21 14:11:38 +0000

    feat: use existing secret

--- postgresql/gen-yaml/gen.sh
+++ postgresql/gen-yaml/gen.sh
@@ -15,6 +15,9 @@ helm template "postgresql" bitnami/postgresql --version "${BITNAMI_POSTGRES_RELE
   --set commonAnnotations."app\.uw\.systems\/description"="" \
   --set commonAnnotations."app\.uw\.systems\/tier"="" \
   --set commonAnnotations."app\.uw\.systems\/repos"="https://github.com/utilitywarehouse/shared-kustomize-bases/tree/main/postgresql" \
+  --set auth.existingSecret="postgresql" \
+  --set auth.secretKeys.adminPasswordKey="admin-password" \
+  --set auth.secretKeys.userPasswordKey="user-password" \
   --set master.resources.requests.cpu="0" \
   --set master.resources.limits.cpu="1000m" \
   --set master.resources.requests.memory="0" \
@@ -22,4 +25,3 @@ helm template "postgresql" bitnami/postgresql --version "${BITNAMI_POSTGRES_RELE
   --set backup.enabled="false" > manifests/upstream/postgres.yaml
 
 cp gen-yaml/kustomization.yaml manifests/upstream/kustomization.yaml
-cp gen-yaml/delete-secret.yaml manifests/upstream/delete-secret.yaml
