968b741e41a788392f41d3b8116ca2305c0ef191
marcinginszt <marcinginszt@gmail.com>
2024-01-18 11:07:47 +0100

    split elasticsearch into two manifests

---
+++ elasticsearch/README.md
@@ -0,0 +1,4 @@
+single node: 
+- scale all nodes but master to 0
+- set env ELASTICSEARCH_IS_DEDICATED_NODE to "false" 
+multi node:
--- elasticsearch/gen-yaml/gen.sh
+++ elasticsearch/gen-yaml/gen.sh
@@ -5,6 +5,8 @@ set -o errexit -o pipefail -o nounset
 # Fetch from helm repo: https://github.com/bitnami/charts/tree/main/bitnami/elasticsearch
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
+
+echo "generate master-only ES manifest"
 helm template "elasticsearch" bitnami/elasticsearch --version "${BITNAMI_ES_RELEASE}" \
   --set fullnameOverride="elasticsearch" \
   --set nameOverride="elasticsearch" \
@@ -12,13 +14,23 @@ helm template "elasticsearch" bitnami/elasticsearch --version "${BITNAMI_ES_RELE
   --set auth.existingSecret="elasticsearch" \
   --set global.kibanaEnabled="true" \
   --set metrics.enabled="true" \
-  --set master.persistence.size="10Gi" \
-  --set master.resources.requests.cpu="500m" \
-  --set master.resources.limits.cpu="1000m" \
-  --set master.resources.requests.memory="1Gi" \
-  --set master.resources.limits.memory="2Gi" \
-  --set data.resources.requests.cpu="500m" \
-  --set data.resources.limits.cpu="1000m" \
-  --set data.resources.requests.memory="1Gi" \
-  --set data.resources.limits.memory="2Gi" \
-  > "manifests/elasticsearch.yaml"
+  --set master.replicaCount="1" \
+  --set master.masterOnly="true" \
+  --set data.replicaCount="0" \
+  --set coordinating.replicaCount="0" \
+  --set ingest.replicaCount="0" \
+  > "manifests/masterOnly/elasticsearch.yaml"
+
+echo "generate ES manifest with specialized node roles"
+helm template "elasticsearch" bitnami/elasticsearch --version "${BITNAMI_ES_RELEASE}" \
+  --set fullnameOverride="elasticsearch" \
+  --set nameOverride="elasticsearch" \
+  --set commonAnnotations."app\.uw\.systems\/repos"="https://github.com/utilitywarehouse/shared-kustomize-bases/tree/main/elasticsearch" \
+  --set auth.existingSecret="elasticsearch" \
+  --set global.kibanaEnabled="true" \
+  --set metrics.enabled="true" \
+  --set master.replicaCount="1" \
+  --set data.replicaCount="1" \
+  --set coordinating.replicaCount="1" \
+  --set ingest.replicaCount="1" \
+  > "manifests/roles/elasticsearch.yaml"
