925b13fee75de187e10b714c5bb73a7bca2dee8e
marcinginszt <marcinginszt@gmail.com>
2024-01-18 13:11:34 +0100

    remove the multi node role cluster

--- elasticsearch/gen-yaml/gen.sh
+++ elasticsearch/gen-yaml/gen.sh
@@ -5,8 +5,6 @@ set -o errexit -o pipefail -o nounset
 # Fetch from helm repo: https://github.com/bitnami/charts/tree/main/bitnami/elasticsearch
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
-
-echo "generate master-only ES manifest"
 helm template "elasticsearch" bitnami/elasticsearch --version "${BITNAMI_ES_RELEASE}" \
   --set fullnameOverride="elasticsearch" \
   --set nameOverride="elasticsearch" \
@@ -14,23 +12,13 @@ helm template "elasticsearch" bitnami/elasticsearch --version "${BITNAMI_ES_RELE
   --set auth.existingSecret="elasticsearch" \
   --set global.kibanaEnabled="true" \
   --set metrics.enabled="true" \
-  --set master.replicaCount="1" \
+  --set master.replicaCount="3" \
   --set master.masterOnly="true" \
   --set data.replicaCount="0" \
   --set coordinating.replicaCount="0" \
   --set ingest.replicaCount="0" \
-  > "manifests/masterOnly/elasticsearch.yaml"
-
-echo "generate ES manifest with specialized node roles"
-helm template "elasticsearch" bitnami/elasticsearch --version "${BITNAMI_ES_RELEASE}" \
-  --set fullnameOverride="elasticsearch" \
-  --set nameOverride="elasticsearch" \
-  --set commonAnnotations."app\.uw\.systems\/repos"="https://github.com/utilitywarehouse/shared-kustomize-bases/tree/main/elasticsearch" \
-  --set auth.existingSecret="elasticsearch" \
-  --set global.kibanaEnabled="true" \
-  --set metrics.enabled="true" \
-  --set master.replicaCount="1" \
-  --set data.replicaCount="1" \
-  --set coordinating.replicaCount="1" \
-  --set ingest.replicaCount="1" \
-  > "manifests/roles/elasticsearch.yaml"
+  --set master.resources.requests.cpu="250m" \
+  --set master.resources.limits.cpu="2000m" \
+  --set master.resources.requests.memory="4Gi" \
+  --set master.resources.limits.memory="8Gi" \
+  > "manifests/elasticsearch.yaml"
